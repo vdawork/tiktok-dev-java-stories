65c31debe97c54372d4b01ae7219f04ff2d6bb1f
vdawork@gmail.com <fyxwsda4>
2021-01-14 10:06:55 +0200

    day10 - json serialization

---
+++ src/main/java/json/JsonSerialization.java
@@ -0,0 +1,13 @@
+package json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import day8.PersonDto;
+
+public class JsonSerialization {
+		public static void main(String[] args) throws JsonProcessingException {
+				ObjectMapper om = new ObjectMapper();
+				String person = om.writeValueAsString(new PersonDto("Dima", 34));
+				System.out.println(person);
+		}
+}
