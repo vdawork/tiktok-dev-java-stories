f0e4e5e24cbc0caf17dc8b28691c8bed78a36c01
vdawork@gmail.com <fyxwsda4>
2021-01-18 13:01:11 +0200

    save json to file

---
+++ src/main/java/json/JsonToFile.java
@@ -0,0 +1,17 @@
+package json;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import day8.PersonDto;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JsonToFile {
+		public static void main(String[] args)  throws IOException {
+				File f = new File("person1.json");
+				new ObjectMapper().createGenerator(f, JsonEncoding.UTF8)
+								.writeObject(new PersonDto("Vasya", 18));
+
+		}
+}
