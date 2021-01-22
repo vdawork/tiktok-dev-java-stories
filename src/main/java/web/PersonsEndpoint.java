7d1ba96a19090407d9eb5b125a2a74c26243b6fe
vdawork@gmail.com <fyxwsda4>
2021-01-22 13:53:09 +0200

    implementing basic business logic

--- src/main/java/web/PersonsEndpoint.java
+++ src/main/java/web/PersonsEndpoint.java
@@ -1,19 +1,29 @@
 package web;
 
 import day8.PersonDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/persons")
 public class PersonsEndpoint {
+
+		@Value("${min.age}")
+		int minAge;
+		
 		@GetMapping()
-		public PersonDto[] all() {
-				return new PersonDto[] {
+		public List<PersonDto> all() {
+				return Arrays.stream(new PersonDto[] {
 								new PersonDto("Nick", 23),
 								new PersonDto("Jack", 21),
 								new PersonDto("Mustafa", 12),
-				};
+				}).filter(p -> p.getAge() >= minAge)
+				.collect(Collectors.toList());
 		}
 }
